80bbbc2d69f6968e75888aeda1d346e6af770633
Dorian Roques <dorian-roques@orange.fr>
2022-04-03 21:34:43 +0200

    Fix sprite paths in presets

--- Assets/Scripts/Control/Entities/EntityType.cs
+++ Assets/Scripts/Control/Entities/EntityType.cs
@@ -5,10 +5,21 @@ public enum EntityType
 {
 	HUMAN_1,
 	
+	CHIKEN_1,
+	COW_1,
+	DIPLODOCUS_1,
 	FOX_1,
+	FROG_1,
+	GOAT_1,
 	RABBIT_1,
-	ANIMAL_3,
+	SNAKE_1,
+	T_REX_1,
+	TAPIR_1,
+	TERMITE_1,
 	
+	BEE_HIVE_1,
+	BUSH_1,
+	FLOWER_1,
+	MOSQUITO_1,
 	TREE_1,
-	BUSH_1
 }
