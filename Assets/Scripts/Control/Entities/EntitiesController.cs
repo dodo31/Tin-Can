4ad3ebac997c1359089f981210e85898305097e7
Hiku <hikuku@hotmail.fr>
2022-04-05 02:00:17 +0200

    Premier jet début partie oeuf

--- Assets/Scripts/Control/Entities/EntitiesController.cs
+++ Assets/Scripts/Control/Entities/EntitiesController.cs
@@ -3,45 +3,43 @@ using UnityEngine;
 
 public class EntitiesController : MonoBehaviour
 {
-	public HumansFactory HumansFactory;
-	public VegetalsFactory VegetalsFactory;
-	public AnimalsFactory AnimalsFactory;
-	public EggsFactory EggsFactory;
+    public HumansFactory HumansFactory;
+    public VegetalsFactory VegetalsFactory;
+    public AnimalsFactory AnimalsFactory;
+    public EggsFactory EggsFactory;
 
-	public EggsController EggsController;
+    public EggsController EggsController;
 
-	private EntityPresetBase _presetBase;
+    private EntityPresetBase _presetBase;
 
-	private Human _player;
+    private Human _player;
 
-	public event Action<Vector3> OnHumanMoved;
+    public event Action<Vector3> OnHumanMoved;
 
-	public event Action<Entity> OnEntitySpawned;
-	public event Action<Entity> OnEntityKilled;
+    public event Action<Entity> OnEntitySpawned;
+    public event Action<Entity> OnEntityKilled;
 
-	public event Action OnPlayerKilled;
+    public event Action OnPlayerKilled;
 
-	protected void Awake()
-	{
-		VegetalSpawnButton[] vegetalSpawnButtons = GameObject.FindObjectsOfType<VegetalSpawnButton>();
-		AnimalSpawnButton[] animalSpawnButtons = GameObject.FindObjectsOfType<AnimalSpawnButton>();
+    protected void Awake()
+    {
+        VegetalSpawnButton[] vegetalSpawnButtons = GameObject.FindObjectsOfType<VegetalSpawnButton>();
+        AnimalSpawnButton[] animalSpawnButtons = GameObject.FindObjectsOfType<AnimalSpawnButton>();
 
-		_presetBase = EntityPresetBase.GetInstance();
-		_player = null;
+        _presetBase = EntityPresetBase.GetInstance();
+        _player = null;
 
-		EggsController.OnEggOdered += this.SpawnEgg;
-	}
+        EggsController.OnEggOdered += this.SpawnEgg;
+    }
 
-	protected void Start()
-	{
-		_player = this.SpawnHuman(EntityType.HUMAN_1, new Vector3(0, 0, 0));
+    protected void Start()
+    {
+        _player = this.SpawnHuman(EntityType.HUMAN_1, new Vector3(0, 0, 0));
 
 
-		float sizeX = 45;
-		float sizeY = 25;
+        /*
 		EntityType[] animals = { EntityType.RABBIT_1, EntityType.FOX_1, EntityType.CHIKEN_1, EntityType.COW_1, EntityType.FROG_1 , EntityType.T_REX_1};
 		int[] amounts = { 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20};
-		System.Random random = new System.Random();
 			for (int j = 0; j < animals.Length; j++)
 			{
 				for (int i = 0; i < amounts[j]; i++)
@@ -51,130 +49,133 @@ public class EntitiesController : MonoBehaviour
 						SpawnAnimal(animals[j], randomPos);
 				}
 			}
+		*/
+        System.Random random = new System.Random();
+        float sizeX = 45;
+        float sizeY = 25;
+        EntityType[] plants = { EntityType.FLOWER_1, EntityType.GRASS_1, EntityType.TREE_1 };
+        int[] amounts2 = { 15, 70, 30, 100, 50, 20 };
+        for (int j = 0; j < plants.Length; j++)
+        {
+            for (int i = 0; i < amounts2[j]; i++)
+            {
+                Vector3 randomPos = new Vector3((float)(-sizeX + sizeX * 2 * random.NextDouble()), (float)(-sizeY + sizeY * 2 * random.NextDouble()), 0);
+                if (Physics2D.OverlapPoint(randomPos, 1 << 6) == null)
+                    SpawnVegetal(plants[j], randomPos);
+            }
+        }
+    }
 
-		EntityType[] plants = { EntityType.BUSH_1, EntityType.FLOWER_1, EntityType.GRASS_1, EntityType.TREE_1, EntityType.MOSQUITO_1, EntityType.BEE_HIVE_1 };
-		int[] amounts2 = { 100, 100, 200, 100, 50, 20 };
-		for (int j = 0; j < plants.Length; j++)
-			{
-				for (int i = 0; i < amounts2[j]; i++)
-				{
-					Vector3 randomPos = new Vector3((float)(-sizeX + sizeX * 2 * random.NextDouble()), (float)(-sizeY + sizeY * 2 * random.NextDouble()), 0);
-					if (Physics2D.OverlapPoint(randomPos, 1 << 6) == null)
-						SpawnVegetal(plants[j], randomPos);
-				}
-			}
-	}
+    protected void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                this.SpawnAnimal(EntityType.CHIKEN_1, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), 0));
+            }
+        }
 
-	protected void FixedUpdate()
-	{
-		if (Input.GetKeyDown(KeyCode.P))
-		{
-			for (int i = 0; i < 30; i++)
-			{
-				this.SpawnAnimal(EntityType.CHIKEN_1, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), 0));
-			}
-		}
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this.SpawnVegetal(EntityType.TREE_1, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), 0));
+            }
+        }
+    }
 
-		if (Input.GetKeyDown(KeyCode.T))
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				this.SpawnVegetal(EntityType.TREE_1, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), 0));
-			}
-		}
-	}
+    public Human SpawnHuman(EntityType type, Vector3 position)
+    {
+        Human newHuman = HumansFactory.CreateHuman(type);
+        this.SpawnEntity(newHuman, position);
+        newHuman.OnMoved += this.OnHumanMoved;
 
-	public Human SpawnHuman(EntityType type, Vector3 position)
-	{
-		Human newHuman = HumansFactory.CreateHuman(type);
-		this.SpawnEntity(newHuman, position);
-		newHuman.OnMoved += this.OnHumanMoved;
+        return newHuman;
+    }
 
-		return newHuman;
-	}
+    public void SpawnVegetal(EntityType type, Vector3 position)
+    {
+        Vegetal newVegetal = VegetalsFactory.CreateVegetal(type);
+        this.SpawnEntity(newVegetal, position);
+        newVegetal.OnBirth += this.SpawnVegetal;
+    }
 
-	public void SpawnVegetal(EntityType type, Vector3 position)
-	{
-		Vegetal newVegetal = VegetalsFactory.CreateVegetal(type);
-		this.SpawnEntity(newVegetal, position);
-		newVegetal.OnBirth += this.SpawnVegetal;
-	}
+    public void SpawnAnimal(EntityType type, Vector3 position)
+    {
+        Animal newAnimal = AnimalsFactory.CreateAnimal(type);
+        this.SpawnEntity(newAnimal, position);
+        newAnimal.OnBirth += this.SpawnAnimal;
+    }
 
-	public void SpawnAnimal(EntityType type, Vector3 position)
-	{
-		Animal newAnimal = AnimalsFactory.CreateAnimal(type);
-		this.SpawnEntity(newAnimal, position);
-		newAnimal.OnBirth += this.SpawnAnimal;
-	}
+    public void SpawnEgg(EntityType type)
+    {
+        EntityPreset entityPreset = _presetBase[type];
+        Vector3 playerPosition = _player.transform.position;
 
-	public void SpawnEgg(EntityType type)
-	{
-		EntityPreset entityPreset = _presetBase[type];
-		Vector3 playerPosition = _player.transform.position;
+        if (entityPreset is VegetalPreset)
+        {
+            this.SpawnVegetalEgg(type, playerPosition);
+        }
+        else if (entityPreset is AnimalPreset)
+        {
+            this.SpawnAnimalEgg(type, playerPosition);
+        }
+    }
 
-		if (entityPreset is VegetalPreset)
-		{
-			this.SpawnVegetalEgg(type, playerPosition);
-		}
-		else if (entityPreset is AnimalPreset)
-		{
-			this.SpawnAnimalEgg(type, playerPosition);
-		}
-	}
+    public void SpawnVegetalEgg(EntityType type, Vector3 position)
+    {
+        Egg newEgg = EggsFactory.CreateVegetalEgg(type);
+        this.SpawnEntity(newEgg, position);
 
-	public void SpawnVegetalEgg(EntityType type, Vector3 position)
-	{
-		Egg newEgg = EggsFactory.CreateVegetalEgg(type);
-		this.SpawnEntity(newEgg, position);
+        newEgg.OnDeath += (Entity entityToKill) =>
+        {
+            this.SpawnVegetal(type, position);
+        };
+    }
 
-		newEgg.OnDeath += (Entity entityToKill) =>
-		{
-			this.SpawnVegetal(type, position);
-		};
-	}
+    public void SpawnAnimalEgg(EntityType type, Vector3 position)
+    {
+        Egg newEgg = EggsFactory.CreateAnimalEgg(type);
+        this.SpawnEntity(newEgg, position);
 
-	public void SpawnAnimalEgg(EntityType type, Vector3 position)
-	{
-		Egg newEgg = EggsFactory.CreateAnimalEgg(type);
-		this.SpawnEntity(newEgg, position);
+        newEgg.OnDeath += (Entity entityToKill) =>
+        {
+            this.SpawnAnimal(type, position);
+        };
+    }
 
-		newEgg.OnDeath += (Entity entityToKill) =>
-		{
-			this.SpawnAnimal(type, position);
-		};
-	}
+    private void SpawnEntity(Entity newEntity, Vector3 position)
+    {
+        newEntity.transform.SetParent(transform);
+        newEntity.transform.position = position;
+        newEntity.OnDeath += this.KillEntity;
 
-	private void SpawnEntity(Entity newEntity, Vector3 position)
-	{
-		newEntity.transform.SetParent(transform);
-		newEntity.transform.position = position;
-		newEntity.OnDeath += this.KillEntity;
+        OnEntitySpawned?.Invoke(newEntity);
+    }
 
-		OnEntitySpawned?.Invoke(newEntity);
-	}
+    public void KillEntity(Entity entityToKill)
+    {
+        foreach (Entity entity in this.GetEntities())
+        {
+            if (entity is Animal animal)
+            {
+                animal.RemoveCloseEntity(entityToKill);
+            }
+        }
 
-	public void KillEntity(Entity entityToKill)
-	{
-		foreach (Entity entity in this.GetEntities())
-		{
-			if (entity is Animal animal)
-			{
-				animal.RemoveCloseEntity(entityToKill);
-			}
-		}
+        if (entityToKill.transform == _player.transform)
+        {
+            OnPlayerKilled?.Invoke();
+        }
 
-		if (entityToKill.transform == _player.transform)
-		{
-			OnPlayerKilled?.Invoke();
-		}
+        OnEntityKilled?.Invoke(entityToKill);
 
-		OnEntityKilled?.Invoke(entityToKill);
+        DestroyImmediate(entityToKill.gameObject);
+    }
 
-		DestroyImmediate(entityToKill.gameObject);
-	}
-
-	private Entity[] GetEntities()
-	{
-		return this.GetComponentsInChildren<Entity>();
-	}
+    private Entity[] GetEntities()
+    {
+        return this.GetComponentsInChildren<Entity>();
+    }
 }
--- Assets/Scripts/Model/Entities/Eggs/EggsSequenceFactory.cs
+++ Assets/Scripts/Model/Entities/Eggs/EggsSequenceFactory.cs
@@ -5,24 +5,41 @@ public static class EggsSequenceFactory
 	public static Queue<EggPool> CreateNewSequence()
 	{
 		Queue<EggPool> _sequence = new Queue<EggPool>();
-		_sequence.Enqueue(new EggPool(1, EntityType.CHIKEN_1, 1));
-		_sequence.Enqueue(new EggPool(2, EntityType.COW_1, 1));
-		_sequence.Enqueue(new EggPool(3, EntityType.DIPLODOCUS_1, 1));
-		_sequence.Enqueue(new EggPool(4, EntityType.FOX_1, 1));
-		_sequence.Enqueue(new EggPool(5, EntityType.FROG_1, 1));
-		_sequence.Enqueue(new EggPool(6, EntityType.GOAT_1, 1));
-		_sequence.Enqueue(new EggPool(7, EntityType.RABBIT_1, 1));
-		_sequence.Enqueue(new EggPool(8, EntityType.SNAKE_1, 1));
-		_sequence.Enqueue(new EggPool(9, EntityType.T_REX_1, 1));
-		_sequence.Enqueue(new EggPool(10, EntityType.TAPIR_1, 1));
-		_sequence.Enqueue(new EggPool(11, EntityType.TERMITE_1, 1));
-		_sequence.Enqueue(new EggPool(12, EntityType.BEAR_1, 1));
-		_sequence.Enqueue(new EggPool(13, EntityType.BEE_HIVE_1, 1));
-		_sequence.Enqueue(new EggPool(14, EntityType.BUSH_1, 1));
-		_sequence.Enqueue(new EggPool(15, EntityType.FLOWER_1, 1));
-		_sequence.Enqueue(new EggPool(16, EntityType.MOSQUITO_1, 1));
-		_sequence.Enqueue(new EggPool(17, EntityType.TREE_1, 1));
-		_sequence.Enqueue(new EggPool(18, EntityType.GRASS_1, 1));
+		_sequence.Enqueue(new EggPool(0, EntityType.GRASS_1, 10));
+		_sequence.Enqueue(new EggPool(30, EntityType.RABBIT_1, 5));
+		_sequence.Enqueue(new EggPool(60, EntityType.TREE_1, 5));
+		_sequence.Enqueue(new EggPool(90, EntityType.CHIKEN_1, 5));
+		_sequence.Enqueue(new EggPool(120, EntityType.COW_1, 4));
+		_sequence.Enqueue(new EggPool(150, EntityType.GRASS_1, 20));
+		_sequence.Enqueue(new EggPool(200, EntityType.FOX_1, 8));
+		_sequence.Enqueue(new EggPool(250, EntityType.BUSH_1, 20));
+		_sequence.Enqueue(new EggPool(300, EntityType.SNAKE_1, 8));
+
+		/*
+		 * 	
+	
+	RABBIT_1,
+	CHIKEN_1,
+	COW_1,
+	FOX_1,
+	SNAKE_1,
+	GOAT_1,
+	FROG_1,
+	TERMITE_1,
+	TAPIR_1,
+	BEAR_1,
+	DIPLODOCUS_1,
+	T_REX_1,
+	
+	GRASS_1,
+	TREE_1,
+	BUSH_1,
+	FLOWER_1,
+	MOSQUITO_1,
+	BEE_HIVE_1,
+
+		 * 
+		 */
 		return _sequence;
 	}
 }
